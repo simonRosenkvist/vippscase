b46ecb6d4d2e94ce807010ab2828f5b3d79c3a4f
simonRosenkvist <sr222qd@student.lnu.se>
2019-11-11 09:09:01 +0100

    added register form (basic)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React, {Component} from 'react';
 import {Elements, StripeProvider} from 'react-stripe-elements';
 import CheckoutForm from './CheckoutForm.js';
 import LoginForm from './LoginForm.js';
+import RegisterForm from './RegisterForm.js';
 
 class App extends Component {
 
   render() {
     return (
       <div>
+      <RegisterForm />
+      <br/>
         <LoginForm />
         <StripeProvider apiKey=" pk_test_AtSP8PNrjvdY0irSoVFsaH4P00GrVSyWv8">
           <div className="example">
---
+++ src/RegisterForm.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+
+class RegisterForm extends React.Component {
+	state = {
+		email: "",
+		password: ""
+	}
+
+	onChangeEmail = (event) => {
+		let val = event.target.value;
+		this.setState({ email: val });
+	}
+
+	onChangePassword = (event) => {
+		let val = event.target.value;
+		this.setState({ password: val });
+	}
+
+	onSubmitForm = (event) => {
+		event.preventDefault();
+		console.log('sending email: ' + this.state.email + ' and password: ' + this.state.password + ' to server, remember to salt and hash password on server!');
+	}
+
+	render() {
+	return (
+		<div>
+			<form>
+				<label>Email: </label>
+				<input type="text" placeholder="Email" onChange={this.onChangeEmail} />
+					<br/>
+				<label>Password: </label>
+				<input type="password" placeholder="Password" onChange={this.onChangePassword} />
+					<br/>
+				<input type="submit" onClick={this.onSubmitForm} />
+			</form>
+		</div>
+	);
+	}
+}
+
+export default RegisterForm;
